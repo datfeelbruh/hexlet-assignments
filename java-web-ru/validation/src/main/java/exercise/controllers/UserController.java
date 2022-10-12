858008dc295b569bccbbe6e92615f02179285e74
@hexlet/cli <support@hexlet.io>
2022-10-12 16:32:37 +0300

    submit java-web-ru/validation

---
+++ java-web-ru/validation/src/main/java/exercise/controllers/UserController.java
@@ -0,0 +1,94 @@
+package exercise.controllers;
+
+import io.javalin.http.Handler;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import io.javalin.core.validation.Validator;
+import io.javalin.core.validation.ValidationError;
+import io.javalin.core.validation.JavalinValidation;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.apache.commons.lang3.StringUtils;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+
+public final class UserController {
+
+    public static Handler listUsers = ctx -> {
+
+        List<User> users = new QUser()
+            .orderBy()
+                .id.asc()
+            .findList();
+
+        ctx.attribute("users", users);
+        ctx.render("users/index.html");
+    };
+
+    public static Handler showUser = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        User user = new QUser()
+            .id.equalTo(id)
+            .findOne();
+
+        ctx.attribute("user", user);
+        ctx.render("users/show.html");
+    };
+
+    public static Handler newUser = ctx -> {
+
+        ctx.attribute("errors", Map.of());
+        ctx.attribute("user", Map.of());
+        ctx.render("users/new.html");
+    };
+
+    public static Handler createUser = ctx -> {
+        // BEGIN
+        String firstName = ctx.formParam("firstName");
+        String lastName = ctx.formParam("lastName");
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
+
+        User user = new User(firstName, lastName, email, password);
+
+        Validator<String> firstNameValidator = ctx.formParamAsClass("firstName", String.class)
+                .check(x -> !x.isEmpty(), "Имя не должно быть пустым");
+
+        Validator<String> lastNameValidator = ctx.formParamAsClass("lastName", String.class)
+                .check(x -> !x.isEmpty(), "Фамилия не должна быть пустой");
+
+        Validator<String> emailValidator = ctx.formParamAsClass("email", String.class)
+                .check(x -> {
+                    EmailValidator validator = EmailValidator.getInstance();
+                    return validator.isValid(x);
+                }, "Введите корректный email");
+        Validator<String> passwordValidator = ctx.formParamAsClass("password", String.class)
+                .check(x -> !x.isEmpty(), "Пароль не должен быть пустым")
+                .check(StringUtils::isNumeric, "В пароле допустимы только цифры")
+                .check(x -> x.length() >= 4, "Пароль не может быть короче 4 символов");
+
+        Map<String, List<ValidationError<? extends Object>>> errors = JavalinValidation.collectErrors(
+                firstNameValidator,
+                lastNameValidator,
+                emailValidator,
+                passwordValidator
+        );
+
+        if (!errors.isEmpty()) {
+            ctx.status(422);
+            ctx.attribute("errors", errors);
+            ctx.attribute("user", user);
+            ctx.sessionAttribute("flash", "Некорректные данные для пользователя");
+            ctx.render("users/new.html");
+            return;
+        }
+
+        user.save();
+        ctx.sessionAttribute("flash", "Пользователь успешно создан");
+        ctx.redirect("/users");
+        // END
+    };
+}

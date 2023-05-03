37ee84278a06190c272ddf5ee10f8eb84d819277
@hexlet/cli <support@hexlet.io>
2023-05-03 22:27:14 +0300

    submit java-advanced-ru/test-containers

--- java-advanced-ru/test-containers/src/test/java/exercise/AppTest.java
+++ java-advanced-ru/test-containers/src/test/java/exercise/AppTest.java
@@ -25,7 +25,8 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @AutoConfigureMockMvc
 
 // BEGIN
-
+@Testcontainers
+@Transactional
 // END
 public class AppTest {
 
@@ -33,7 +34,19 @@ public class AppTest {
     private MockMvc mockMvc;
 
     // BEGIN
-    
+    @Container
+    private static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres")
+            .withDatabaseName("dbname")
+            .withUsername("sa")
+            .withPassword("sa")
+            .withInitScript("init.sql;");
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+    }
     // END
 
     @Test
@@ -58,4 +71,81 @@ public class AppTest {
         assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
         assertThat(response.getContentAsString()).contains("Jackson", "Bind");
     }
+
+
+    @Test
+    void testGetPeoples() throws Exception {
+        MockHttpServletResponse responseGet = mockMvc
+                .perform(
+                        get("/people")
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(responseGet.getStatus()).isEqualTo(200);
+        assertThat(responseGet.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
+        assertThat(responseGet.getContentAsString()).contains("John", "Smith");
+    }
+
+    @Test
+    void testGetPeople() throws Exception {
+        MockHttpServletResponse responseGet = mockMvc
+                .perform(
+                        get("/people/2")
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(responseGet.getStatus()).isEqualTo(200);
+
+
+        assertThat(responseGet.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
+        assertThat(responseGet.getContentAsString()).contains("Jack", "Doe");
+    }
+
+    @Test
+    void testDeletePeople() throws Exception {
+        MockHttpServletResponse responseDelete = mockMvc
+                .perform(
+                        delete("/people/1")
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(responseDelete.getStatus()).isEqualTo(200);
+
+        MockHttpServletResponse response = mockMvc
+                .perform(get("/people"))
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
+        assertThat(response.getContentAsString()).doesNotContain("John", "Smith");
+    }
+
+    @Test
+    void testUpdatePeople() throws Exception {
+        MockHttpServletResponse responsePatch = mockMvc
+                .perform(
+                        patch("/people/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"firstName\": \"Dmitriy\", \"lastName\": \"Khoroshko\"}")
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(responsePatch.getStatus()).isEqualTo(200);
+
+        MockHttpServletResponse response = mockMvc
+                .perform(get("/people"))
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON.toString());
+        assertThat(response.getContentAsString()).contains("Dmitriy", "Khoroshko");
+    }
+
 }
